66c65d9599c2f97b2e9411daa343a478d1913a2f
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-07-13 16:39:14 +0800

    init

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ driver = webdriver.Chrome(PATH)
 
 driver.get("https://www.instagram.com/")
 
+Sel_user = browser.findElement(By.cssSelector, username_selector)
+
+
 # login
 time.sleep(5)
 username = driver.find_element_by_css_selector("input[name='username']")
@@ -39,48 +42,48 @@ time.sleep(5)
 searchbox.send_keys(Keys.ENTER)
 
 
-# # scroll
-# scrolldown = driver.execute_script(
-#     "window.scrollTo(0, document.body.scrollHeight);var scrolldown=document.body.scrollHeight;return scrolldown;"
-# )
-# match = False
-# while match == False:
-#     last_count = scrolldown
-#     time.sleep(3)
-#     scrolldown = driver.execute_script(
-#         "window.scrollTo(0, document.body.scrollHeight);var scrolldown=document.body.scrollHeight;return scrolldown;"
-#     )
-#     if last_count == scrolldown:
-#         match = True
+# scroll
+scrolldown = driver.execute_script(
+    "window.scrollTo(0, document.body.scrollHeight);var scrolldown=document.body.scrollHeight;return scrolldown;"
+)
+match = False
+while match == False:
+    last_count = scrolldown
+    time.sleep(3)
+    scrolldown = driver.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);var scrolldown=document.body.scrollHeight;return scrolldown;"
+    )
+    if last_count == scrolldown:
+        match = True
 
-# # posts
-# posts = []
-# links = driver.find_elements_by_tag_name("a")
-# for link in links:
-#     post = link.get_attribute("href")
-#     if "/p/" in post:
-#         posts.append(post)
+# posts
+posts = []
+links = driver.find_elements_by_tag_name("a")
+for link in links:
+    post = link.get_attribute("href")
+    if "/p/" in post:
+        posts.append(post)
 
-# print(posts)
+print(posts)
 
 
-# # get videos and images
-# download_url = ""
-# for post in posts:
-#     driver.get(post)
-#     shortcode = driver.current_url.split("/")[-2]
-#     time.sleep(7)
-#     if (
-#         driver.find_element_by_css_selector("img[style='object-fit: cover;']")
-#         is not None
-#     ):
-#         download_url = driver.find_element_by_css_selector(
-#             "img[style='object-fit: cover;']"
-#         ).get_attribute("src")
-#         urllib.request.urlretrieve(download_url, "{}.jpg".format(shortcode))
-#     else:
-#         download_url = driver.find_element_by_css_selector(
-#             "video[type='video/mp4']"
-#         ).get_attribute("src")
-#         urllib.request.urlretrieve(download_url, "{}.mp4".format(shortcode))
-#     time.sleep(5)
+# get videos and images
+download_url = ""
+for post in posts:
+    driver.get(post)
+    shortcode = driver.current_url.split("/")[-2]
+    time.sleep(7)
+    if (
+        driver.find_element_by_css_selector("img[style='object-fit: cover;']")
+        is not None
+    ):
+        download_url = driver.find_element_by_css_selector(
+            "img[style='object-fit: cover;']"
+        ).get_attribute("src")
+        urllib.request.urlretrieve(download_url, "{}.jpg".format(shortcode))
+    else:
+        download_url = driver.find_element_by_css_selector(
+            "video[type='video/mp4']"
+        ).get_attribute("src")
+        urllib.request.urlretrieve(download_url, "{}.mp4".format(shortcode))
+    time.sleep(5)
